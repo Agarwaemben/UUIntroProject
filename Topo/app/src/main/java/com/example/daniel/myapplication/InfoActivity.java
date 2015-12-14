eff7243000bac46fcd73c1bfd097542715be6659
LaurensAC <laurenscastelijns@gmail.com>
2015-12-14 10:30:41 +0100

    Database structuur toegevoegd

--- Topo/app/src/main/java/com/example/daniel/myapplication/InfoActivity.java
+++ Topo/app/src/main/java/com/example/daniel/myapplication/InfoActivity.java
@@ -2,15 +2,39 @@ package com.example.daniel.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.daniel.myapplication.R;
 
 public class InfoActivity extends Activity {
+
+    TopoHelper Helper = new TopoHelper(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_layout);
+        Button laad;
+        laad = (Button) findViewById(R.id.button4);
+        laad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getStad;
+                getStad = "";
+                Helper.openDataBase();
+                Cursor C = Helper.returndata();
+                Toast.makeText(getBaseContext(), "Database open ",Toast.LENGTH_LONG ).show();
+                if (C.moveToFirst()) {
+                    do {
+                        getStad = C.getString(0);
+                    }while (C.moveToNext());
+                }
+                Helper.close();
+                Toast.makeText(getBaseContext(), "Stad: " + getStad,Toast.LENGTH_LONG ).show();
+            }
+        });
     }
 }
---
+++ Topo/app/src/main/java/com/example/daniel/myapplication/TopoHelper.java
@@ -0,0 +1,116 @@
+package com.example.daniel.myapplication;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.SyncStateContract;
+import android.util.Log;
+
+public class TopoHelper extends SQLiteOpenHelper
+{
+    private static String TAG = "DataBaseHelper"; // Tag voor logcat
+    //locatie van de database op hett apparaat
+    private static String DB_PATH = "";
+    private static String DB_NAME ="TopoBase.db";// Database naam
+    private SQLiteDatabase mDataBase;
+    private final Context mContext;
+
+    public TopoHelper(Context context)
+    {
+        super(context, DB_NAME, null, 1);
+        //path aanpassen aan android versie (gaf problemen met bepaalde versies van Android)
+        if(android.os.Build.VERSION.SDK_INT >= 17){
+            DB_PATH = "/data/data/com.example.daniel.myapplication/databases/";
+        }
+        else
+        {
+            DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
+        }
+        this.mContext = context;
+    }
+
+    public void createDataBase() throws IOException
+    {
+        //Als er geen database is --> lege database aanmaken en de inhoud van TopoBase.db hierin kopieren (uit de assets folder).
+
+        boolean mDataBaseExist = checkDataBase();
+        if(!mDataBaseExist)
+        {
+            this.getReadableDatabase();
+            this.close();
+            try
+            {
+                copyDataBase();
+                Log.e(TAG, "createDatabase database created");
+            }
+            catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    //checken of de database bestaat op /data/data/com.example.daniel.myapplication/databases/TopoBase.db
+    private boolean checkDataBase()
+    {
+        File dbFile = new File(DB_PATH + DB_NAME);
+        return dbFile.exists();
+    }
+
+    //methode om de database te kopieren
+    private void copyDataBase() throws IOException
+    {
+        InputStream mInput = mContext.getAssets().open(DB_NAME);
+        String outFileName = DB_PATH + DB_NAME;
+        OutputStream mOutput = new FileOutputStream(outFileName);
+        byte[] mBuffer = new byte[1024];
+        int mLength;
+        while ((mLength = mInput.read(mBuffer))>0)
+        {
+            mOutput.write(mBuffer, 0, mLength);
+        }
+        mOutput.flush();
+        mOutput.close();
+        mInput.close();
+    }
+
+    //Methode om de database te openen zodat er mee gewerkt kan worden
+    public boolean openDataBase() throws SQLException
+    {
+        String mPath = DB_PATH + DB_NAME;
+        mDataBase = SQLiteDatabase.openDatabase(mPath, null, SQLiteDatabase.CREATE_IF_NECESSARY);
+        return mDataBase != null;
+    }
+
+    @Override
+    public synchronized void close()
+    {
+        if(mDataBase != null)
+            mDataBase.close();
+        super.close();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+    //************
+    //testmethode
+    //************
+    public Cursor returndata(){
+       return mDataBase.query("Kaart_Nederland", new String[]{"Stad"},null,null,null,null,null);
+
+    }
+}

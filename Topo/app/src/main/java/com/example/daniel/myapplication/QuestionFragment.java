7a6a5fc1155b8460f7d3f0bb57bea595519f83bb
Mark Jan <markjanvanlieburg@gmail.com>
2015-12-19 15:32:58 +0100

    stap1 van opruiming

--- Topo/app/src/main/java/com/example/daniel/myapplication/QuestionFragment.java
+++
@@ -1,7 +0,0 @@
-package com.example.daniel.myapplication;
-
-// definieert de functies die een vraagtypefragment moet hebben om te kunnen functioneren
-public interface QuestionFragment {
-    void showQuestion();
-    void showHint();
-}

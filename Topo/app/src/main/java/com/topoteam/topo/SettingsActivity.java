bc58cad176462bdd109e006449e9c924f9899c62
Machteld <m.hamers97@gmail.com>
2015-12-21 10:46:05 +0100

    SharedPref's basics.

--- Topo/app/src/main/java/com/topoteam/topo/SettingsActivity.java
+++ Topo/app/src/main/java/com/topoteam/topo/SettingsActivity.java
@@ -3,12 +3,14 @@ package com.topoteam.topo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.view.View;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends PreferenceActivity {
+    @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_layout);
+        addPreferencesFromResource(R.xml.preferences);
     }
 }

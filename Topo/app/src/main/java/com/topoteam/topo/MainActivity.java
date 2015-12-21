129333fe0e4de7dc44c4b94f3790b0cda23e5e2a
Machteld <m.hamers97@gmail.com>
2015-12-21 10:55:20 +0100

    optionsmenu weg

--- Topo/app/src/main/java/com/topoteam/topo/MainActivity.java
+++ Topo/app/src/main/java/com/topoteam/topo/MainActivity.java
@@ -31,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //Optionsmenu verwijderd, omdat we eigen 'Instellingen' knop hebben.
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -52,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
+*/
 
     public void SelectieScherm(View view)
     {

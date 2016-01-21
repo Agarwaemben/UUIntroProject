2658fb9a0634704fc0e7438aebbc50f63b6df6b0
Felix Hinlopen <f.a.hinlopen@gmail.com>
2016-01-21 15:02:50 +0100

    MainActivity update
    Nu met achtergrond en titel

--- Topo/app/src/main/java/com/topoteam/topo/MainActivity.java
+++ Topo/app/src/main/java/com/topoteam/topo/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageView;
 
 import java.io.IOException;
 
@@ -22,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        ImageView v = (ImageView)findViewById(R.id.main_logo);
+        v.setImageResource(R.drawable.titel2);
     }
     //Optionsmenu verwijderd, omdat we eigen 'Instellingen' knop hebben.
 
